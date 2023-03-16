15dc521711d0688a58857a39a35332c56f599911
Kim Barry <kimbarrymail@gmail.com>
2023-03-16 14:29:36 -0400

    initial commit modules 1-3

---
+++ CityInfo.API/CityInfo.API/Controllers/FilesController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(
+                    nameof(fileExtensionContentTypeProvider));
+        }
+        
+        
+        
+        
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            //look up actual file depending on fileId
+            var pathToFile = "Kim_Barry_Resume_2023.pdf";
+
+            //check if file exists
+            if(!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";//default content type for binary media
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+    }
+}
