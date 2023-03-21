de879b7786af8b6d67de2a2ae2cfb1da31ac8021
Kim Barry <kimbarrymail@gmail.com>
2023-03-21 16:59:44 -0400

    modules 6.8-6.9 safely storing connection strings

--- CityInfo.API/CityInfo.API/Program.cs
+++ CityInfo.API/CityInfo.API/Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddTransient<IMailService, CloudMailService>();
 
 builder.Services.AddSingleton<CitiesDataStore>();
 
-builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite("Data Source=CityInfo.db"));
+builder.Services.AddDbContext<CityInfoContext>(dbContextOptions => dbContextOptions.UseSqlite(builder.Configuration["ConnectionStrings:CityInfoDBConnectionString"]));
 
 var app = builder.Build();
 
