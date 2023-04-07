91ba9031206678fac178f8c0afc7c2f61f86fdb1
Kim Barry <kimbarrymail@gmail.com>
2023-04-07 18:41:42 -0400

    modules 10.9-10.12

--- CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
+++ CityInfo.API/CityInfo.API/Controllers/PointsOfInterestController.cs
@@ -10,7 +10,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CityInfo.API.Controllers
 {
     [Route("api/v{version:apiVersion}/cities/{cityId}/pointsofinterest")]
-    //[Authorize]
+    [Authorize]
     [ApiVersion("2.0")]
     [ApiController]
     public class PointsOfInterestController : ControllerBase
